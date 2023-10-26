b0db759cd92d8c0da2b97e0bccaa86569165bff6
unknown <iranmedina@estudiantec.cr>
2023-10-25 18:34:09 -0600

    Prueba driver monitor

--- clases_interface.sv
+++ clases_interface.sv
@@ -98,7 +98,7 @@ class trans_mesh #(
     function print();
 
       $display("El paquete posee los siguiente elementos:");
-      $display("Nex jump: %h \nRow: %h \nColum: %h \nMode: %h \nPayload: %h \nPackage: %h \nTerminal Envio: %g \nTiempo envio: %g \nTerminal recibido: %g \nTiempo recibido: %g \n",
+      $display("Nex jump: %h \nRow: %h \nColum: %h \nMode: %h \nPayload: %h \nPackage: %h \nTerminal Envio: %h \nTiempo envio: %g \nTerminal recibido: %h \nTiempo recibido: %g \n",
         
         this.next_jump,
         this.row,
