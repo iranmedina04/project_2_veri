9514ef4ee8867fb09869b8689908f6e40a362597
unknown <iranmedina@estudiantec.cr>
2023-11-15 00:24:27 -0600

    Arreglo del scoreboard

--- ambiente.sv
+++ ambiente.sv
@@ -166,7 +166,7 @@ class ambiente #(
         
     endfunction
 
-    function  run();
+    task  run();
 
         test_sb_mailbox = new();
         my_agente.test_agente_mbx = test_agente_mbx;
@@ -217,7 +217,7 @@ class ambiente #(
 
         end
         
-    endfunction
+    endtask
 
 
 endclass
