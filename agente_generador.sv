e0c49aa75cfa22edc8ae78188f9da091f7039ef4
unknown <iranmedina@estudiantec.cr>
2023-11-15 20:12:52 -0600

    Prueba del test

--- agente_generador.sv
+++ agente_generador.sv
@@ -47,7 +47,7 @@ class agente #(     parameter ROWS = 4,
         forever begin
             #1
             if (test_agente_mbx.num() > 0)begin //Verifica si hay algo en el mailbox del test al agente
-                //$display("[%g] El agente fue inicializado",$time);
+                $display("[%g] El agente fue inicializado",$time);
                 test_agente_mbx.get(instruccion); //Se obtiene la instrucción del mailbox del test al agente
                 case (instruccion)
                     un_paquete: begin //Caso en el que se envía un solo paquete aleatorio desde cualquier dispositivo hacia cualquier otro dispositivo
